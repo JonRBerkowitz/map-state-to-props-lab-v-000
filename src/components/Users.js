5e7c7cdcab21c875d429c68dd5a601524252c548
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-04-18 15:57:55 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -13,6 +13,7 @@ class Users extends Component {
             </div>
           )}
         </ul>
+        <p>Number of users: { this.props.user_number } </p>
       </div>
     )
   }
@@ -21,7 +22,10 @@ class Users extends Component {
 //add mapStateToProps here
 
 const mapStateToProps = (state) => {
-	return { users: state.users };
+	return {
+    users: state.users,
+    user_number: state.users.length
+  };
 }
 
 export default connect(mapStateToProps)(Users)
